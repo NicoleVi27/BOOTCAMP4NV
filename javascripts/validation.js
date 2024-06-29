8065ba64cd970d1af285a782e05b68c92c0ff996
Milagros Nicole Villalba Espinoza <mvillale@vision.local>
2024-06-29 18:33:43 -0400

    Actualizacion 2

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -1,33 +1,24 @@
+// Función para validar el correo electrónico
 function validacionCorreoElectronico() {
     // Obtener referencias a los elementos del formulario
     const formulario = document.getElementById('formulario');
     const correo = document.getElementById('correo');
     const correoConfirmar = document.getElementById('correo_confirmar');
-    
+    const mensajeError = document.getElementById('mensaje-error-correo');
+
     // Agregar un evento para cuando se ingrese texto en el campo de confirmación de correo electrónico
     correoConfirmar.addEventListener('input', () => {
-        const mensajeError = document.getElementById('mensaje-error-correo');
-        
         // Comprobar si los correos electrónicos coinciden
         if (correo.value !== correoConfirmar.value) {
-            // Si no coinciden y no hay un mensaje de error, crear y mostrar uno
-            if (!mensajeError) {
-                const elemento = document.createElement('p');
-                const mensaje = document.createTextNode("Los correos electrónicos no coinciden");
-                elemento.appendChild(mensaje);
-                elemento.id = 'mensaje-error-correo';
-                elemento.classList.add('alerta');
-                
-                const correoConfirmarTd = correoConfirmar.parentElement;
-                correoConfirmarTd.appendChild(elemento);
-            }
-            // Cambiar el color de fondo del campo de confirmación de correo electrónico
-            correoConfirmar.style.backgroundColor = 'rgba(230,169,171,.5)';
+            // Si no coinciden, mostrar mensaje de error y cambiar color de fondo
+            mensajeError.textContent = "Email does not match";
+            mensajeError.style.color = '#d14539';  // Aplicar color de texto #d14539
+            mensajeError.style.display = 'block';
+            correoConfirmar.style.backgroundColor = 'rgba(230, 169, 171, 0.5)';  // Cambiar color de fondo
         } else {
-            // Si los correos electrónicos coinciden, eliminar el mensaje de error (si existe) y restablecer el color de fondo
-            if (mensajeError) {
-                mensajeError.remove();
-            }
+            // Si coinciden, eliminar el mensaje de error y restablecer color de fondo
+            mensajeError.style.display = 'none';
+            mensajeError.textContent = '';
             correoConfirmar.style.backgroundColor = '';
         }
     });
